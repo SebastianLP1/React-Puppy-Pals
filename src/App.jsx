e8e710df8429518b89b1a6553c55c0255415f7a8
algorithm_artisit <algorithm_artist@DESKTOP-SV23GD5>
2023-11-26 08:36:56 -0800

    finished with step styling error trying to fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 
+import './App.css'
+import {puppyList} from './data.js'
+import { useState } from 'react'
+
 
 
 
   
-      <div>
-        
-      </div>
-    
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
+  function handleClick() {
+    // some logic here
+  }
+
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p onClick= {()=>{console.log("puppy id: ", puppy.id)}} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App
